f450f13f9cbe03eafc21b4413c7fb0a39eef5764
yanchen <rtaromax@hotmail.com>
2020-05-21 14:27:36 +0800

    update

--- contract_generator.py
+++ contract_generator.py
@@ -31,32 +31,35 @@ def create_layout(app):
                     html.Div(
                     	html.Div(
 	                        [
-	                        contract_gen_basic(app),
-	                        contract_gen_parameter(app),
-	                        html.Div(style={"height":"20px"}),
-	                        contract_gen_measure(app),
-							dbc.Row([
-								dbc.Col(
+	                        	html.H1("Contract Generator", style={"padding-left":"20px","padding-bottom":"30px"}),
+		                        contract_gen_basic(app),
+		                        html.Div(style={"height":"20px"}),
+		                        contract_gen_parameter(app),
+		                        html.Div(style={"height":"40px"}),
+		                        contract_gen_measure(app),
+		                        html.Div(style={"height":"20px"}),
+								html.Div(
 									dcc.Upload(
-									id = 'upload-data',
-									children = html.Div([
-										'Select Contract Template to Upload'
-										],style={"font-family":"NotoSans-Regular","font-size":"1rem","text-decoration":"underline","color":"#1357DD"}),
-									style={
-										'height': '60px',
-										'lineHeight': '60px',
-										'borderWidth': '1px',
-										'borderStyle': 'dashed',
-										'borderRadius': '5px',
-										'textAlign': 'center',
-										'margin': '10px'
-										}
-									),style={"padding-top":"1rem"}, width=12),
-								]),
-							html.Div(dbc.Button('Generate Contract', style={"text-align":"center", "background-color":"#381610", "border-radius":"10rem"}), style={"padding-bottom":"20px"})
-
-	                                
-	                        ]
+										id = 'upload-data',
+										children = html.Div([
+											'Select Contract Template to Upload'
+											],style={"font-family":"NotoSans-Regular","font-size":"1rem","text-decoration":"underline","color":"#1357DD"}),
+										style={
+											'height': '60px',
+											'lineHeight': '60px',
+											'borderWidth': '1px',
+											'borderStyle': 'dashed',
+											'borderRadius': '5px',
+											'textAlign': 'center',
+											'margin': '10px'
+											}
+									),
+									style={"padding":"20px","background-color":"#f2f7ff"}
+								),
+								html.Div(style={"height":"20px"}),
+								html.Div(dbc.Button('Generate Contract', style={"text-align":"center", "background-color":"#381610", "border-radius":"10rem"}), style={"padding-bottom":"40px", "text-align":"center"})
+							]      
+	                        
 	                    ),
                         className="mb-3",
                     	style={"box-shadow":"0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 20px 0 rgba(0, 0, 0, 0.05)","background-color":"#fff","padding":"20px","padding-bottom":"0rem", "width":"850px", "margin":"auto"},
@@ -66,105 +69,126 @@ def create_layout(app):
 
 def contract_gen_basic(app):
 	return html.Div([
-		dbc.Row([
-			dbc.Col('Plan Year', width=7),
-			dbc.Col([
-				dbc.InputGroup([
-					dbc.Input(value = '1/1/2020'),
-					dbc.InputGroupAddon('-', addon_type = 'append'),
-					dbc.Input(value = '12/31/2020'),
-					],size="sm")
-				], width=5)
-			],
-			style={"padding-bottom":"10px"}),
-		dbc.Row([
-			dbc.Col("ACO's name", width=7),
-			dbc.Col([
-				dbc.Input(value = 'ACO1', bs_size="sm")
-				], width=5),
-			]),
+		html.Div(
+			[
+				html.H1("Basic Info", style={"font-size":"1.25rem"}),
+				html.Hr(),
+				dbc.Row([
+					dbc.Col('Contract Period', width=7),
+					dbc.Col([
+						dbc.InputGroup([
+							dbc.Input(value = '1/1/2020', style={"text-align":"center"}),
+							dbc.InputGroupAddon('-', addon_type = 'append'),
+							dbc.Input(value = '12/31/2020', style={"text-align":"center"}),
+							],size="sm")
+						], width=5)
+					],
+					style={"padding-bottom":"10px"}),
+				dbc.Row([
+					dbc.Col("ACO's name", width=7),
+					dbc.Col([
+						dbc.Input(value = 'ACO1', bs_size="sm", style={"text-align":"center"})
+						], width=5),
+					]),
+			]
+		),
+		
 		],
-		style={"padding-bottom":"40px","padding-left":"20px","padding-right":"20px"})
+		style={"padding":"20px","background-color":"#f2f7ff"})
 
 def contract_gen_parameter(app):
 	return html.Div([
-		dbc.Row([
-			dbc.Col('Medical Cost Target PMPM', width=7),
-			dbc.Col([
-				dbc.InputGroup([
-					dbc.InputGroupAddon('$', addon_type = 'prepend'),
-					dbc.Input(value = custom_input['medical cost target']['user target']), 
-					], size="sm"),
-				], width=5)
-			],
-			style={"padding-bottom":"10px"}),
-		html.Hr(),
-		dbc.Row([
-			dbc.Col('Shared Savings'),
-			]),
-		dbc.Row([
-			dbc.Col('MSR (Minimum Savings Rate)', width=7),
-			dbc.Col([
-				dbc.InputGroup([
-					dbc.Input(value = custom_input['savings/losses sharing arrangement']['msr']),
-					dbc.InputGroupAddon('%', addon_type = 'append'),
-					],size="sm"),
-				], width=5)
-			],
-			style={"padding-bottom":"10px"}),
-		dbc.Row([
-			dbc.Col('Max Sharing % (When quality targets are met)', width=7),
-			dbc.Col([
-				dbc.InputGroup([
-					dbc.Input(value = custom_input['savings/losses sharing arrangement']['savings sharing']),
-					dbc.InputGroupAddon('%', addon_type = 'append'),
-					],size="sm"),
-				], width=5)
-			],
-			style={"padding-bottom":"10px"}),
-		dbc.Row([
-			dbc.Col('Min Sharing %', width=7),
-			dbc.Col([
-				dbc.InputGroup([
-					dbc.Input(value = custom_input['savings/losses sharing arrangement']['savings sharing min']),
-					dbc.InputGroupAddon('%', addon_type = 'append'),
-					],size="sm"),
-				], width=5)
-			],
-			style={"padding-bottom":"10px"}),
-		dbc.Row([
-			dbc.Col('Sharing Method', width=7),
-			dbc.Col([
-				dcc.Dropdown(options = [{'label':'First Dollar Sharing', 'value':'First Dollar Sharing'},
-					{'label':'Second Dollar Sharing (Below MLR)', 'value':'Second Dollar Sharing (Below MLR)'}],
-					value = custom_input['savings/losses sharing arrangement']['saving sharing method'],clearable = False),
-
-				], width=5)
-			],
-			style={"padding-bottom":"10px"}),
-		dbc.Row([
-			dbc.Col('Shared Savings Cap', width=7),
-			dbc.Col([
-				dbc.InputGroup([
-					dbc.Input(value = custom_input['savings/losses sharing arrangement']['savings share cap']),
-					dbc.InputGroupAddon('%', addon_type = 'append'),
-					],size="sm"),
-				], width=5)
-			],
-			style={"padding-bottom":"10px"}),
-		html.Div([
+		html.Div(
+			[
+			html.H1("Cost Target", style={"font-size":"1.25rem"}),
 			html.Hr(),
 			dbc.Row([
-				dbc.Col('Shared Losses', width=7),
-				]),
+				dbc.Col('Medical Cost Target PMPM', width=7),
+				dbc.Col([
+					dbc.InputGroup([
+						dbc.InputGroupAddon('$', addon_type = 'prepend'),
+						dbc.Input(value = custom_input['medical cost target']['user target']), 
+						], size="sm"),
+					], width=5)
+				],
+				style={"padding-bottom":"10px"}),
+			],
+			style={"padding":"20px","background-color":"#f2f7ff"}
+		),
+		
+		html.Div(style={"height":"20px"}),
+
+		html.Div(
+			[
+				html.H1("Shared Savings", style={"font-size":"1.25rem"}),
+				html.Hr(),
+				dbc.Row([
+					dbc.Col('MSR (Minimum Savings Rate)', width=7),
+					dbc.Col([
+						dbc.InputGroup([
+							dbc.Input(value = custom_input['savings/losses sharing arrangement']['msr']),
+							dbc.InputGroupAddon('%', addon_type = 'append'),
+							],size="sm"),
+						], width=2)
+					],
+					style={"padding-bottom":"10px"}),
+				dbc.Row([
+					dbc.Col('Max Sharing % (When quality targets are met)', width=7),
+					dbc.Col([
+						dbc.InputGroup([
+							dbc.Input(value = custom_input['savings/losses sharing arrangement']['savings sharing']),
+							dbc.InputGroupAddon('%', addon_type = 'append'),
+							],size="sm"),
+						], width=2)
+					],
+					style={"padding-bottom":"10px"}),
+				dbc.Row([
+					dbc.Col('Min Sharing %', width=7),
+					dbc.Col([
+						dbc.InputGroup([
+							dbc.Input(value = custom_input['savings/losses sharing arrangement']['savings sharing min']),
+							dbc.InputGroupAddon('%', addon_type = 'append'),
+							],size="sm"),
+						], width=2)
+					],
+					style={"padding-bottom":"10px"}),
+				dbc.Row([
+					dbc.Col('Sharing Method', width=7),
+					dbc.Col([
+						dcc.Dropdown(options = [{'label':'First Dollar Sharing', 'value':'First Dollar Sharing'},
+							{'label':'Second Dollar Sharing (Below MLR)', 'value':'Second Dollar Sharing (Below MLR)'}],
+							value = custom_input['savings/losses sharing arrangement']['saving sharing method'],clearable = False,style={"font-size":"0.8rem"}),
+
+						], width=5)
+					],
+					style={"padding-bottom":"10px"}),
+				dbc.Row([
+					dbc.Col('Shared Savings Cap', width=7),
+					dbc.Col([
+						dbc.InputGroup([
+							dbc.Input(value = custom_input['savings/losses sharing arrangement']['savings share cap']),
+							dbc.InputGroupAddon('%', addon_type = 'append'),
+							],size="sm"),
+						], width=2)
+					],
+					style={"padding-bottom":"10px"}),
+			],
+			style={"padding":"20px","background-color":"#f2f7ff"}
+		),
+
+		html.Div(style={"height":"20px"}),
+		
+		html.Div([
+			html.H1("Shared Losses", style={"font-size":"1.25rem"}),
+			html.Hr(),
 			dbc.Row([
-				dbc.Col('MLR (Minimum Losses Rate)'),
+				dbc.Col('MLR (Minimum Losses Rate)', width=7),
 				dbc.Col([
 					dbc.InputGroup([
 						dbc.Input(value = custom_input['savings/losses sharing arrangement']['mlr']),
 						dbc.InputGroupAddon('%', addon_type = 'append'),
 						],size="sm"),
-					], width=5)
+					], width=2)
 				],
 				style={"padding-bottom":"10px"}),
 			dbc.Row([
@@ -174,7 +198,7 @@ def contract_gen_parameter(app):
 						dbc.Input(value = custom_input['savings/losses sharing arrangement']['losses sharing min']),
 						dbc.InputGroupAddon('%', addon_type = 'append'),
 						],size="sm"),
-					], width=5)
+					], width=2)
 				],
 				style={"padding-bottom":"10px"}),
 			dbc.Row([
@@ -184,7 +208,7 @@ def contract_gen_parameter(app):
 						dbc.Input(value = custom_input['savings/losses sharing arrangement']['losses sharing']),
 						dbc.InputGroupAddon('%', addon_type = 'append'),
 						],size="sm"),
-					], width=5)
+					], width=2)
 				],
 				style={"padding-bottom":"10px"}),
 			dbc.Row([
@@ -192,7 +216,7 @@ def contract_gen_parameter(app):
 				dbc.Col([
 					dcc.Dropdown(options = [{'label':'First Dollar Sharing', 'value':'First Dollar Sharing'},
 					{'label':'Second Dollar Sharing (Below MLR)', 'value':'Second Dollar Sharing (Below MLR)'}],
-						value = custom_input['savings/losses sharing arrangement']['loss sharing method'],clearable = False),
+						value = custom_input['savings/losses sharing arrangement']['loss sharing method'],clearable = False,style={"font-size":"0.8rem"}),
 					], width=5)
 				],
 				style={"padding-bottom":"10px"}),
@@ -203,11 +227,14 @@ def contract_gen_parameter(app):
 						dbc.Input(value = custom_input['savings/losses sharing arrangement']['losses share cap']),
 						dbc.InputGroupAddon('%', addon_type = 'append'),
 						],size="sm"),
-					], width=5)
+					], width=2)
 				]),
-			], hidden = not custom_input['savings/losses sharing arrangement']['two side']),
+			], 
+			hidden = not custom_input['savings/losses sharing arrangement']['two side'],
+			style={"padding":"20px","background-color":"#f2f7ff"}),
 		],
-		style={"padding-left":"20px","padding-right":"20px","padding-top":"20px","padding-bottom":"20px","background-color":"#f2f7ff","font-family":"NotoSans-Regular"})
+		style={"font-family":"NotoSans-Regular"}
+	)
 
 def contract_gen_measure(app):
 
@@ -252,10 +279,12 @@ def contract_gen_measure(app):
 	df['Weight'] = weight_list
 
 	return html.Div([
+		html.H1("Quality Measures", style={"font-size":"1.25rem"}),
+		html.Hr(),
 		dash_table.DataTable(
 			data = df.to_dict('records'),
 			columns = [{'name': i, 'id':i} for i in df.columns],
-			style_data = {'textAlign' : 'center','font-family': 'NotoSans-CondensedLight',},
+			style_data = {'textAlign' : 'center','font-family': 'NotoSans-CondensedLight',"font-size":"0.85rem"},
 			style_header = {'backgroundColor': '#f1f6ff',
 			'fontWeight': 'bold',
 			'font-family':'NotoSans-CondensedLight',
@@ -267,9 +296,10 @@ def contract_gen_measure(app):
 				{ 'if': {'column_id':'Measure'}, 
 				 'font-weight':'bold', 
 				 'textAlign': 'start',
-				 'width':'14rem',
+				 'width':'22rem',
 				 'height':'3rem',
-				 'whiteSpace':'normal'
+				 'whiteSpace':'normal',
+				 'padding-left':'10px'
 				 #'minWidth': '25rem',
 				 #'maxWidth':'25rem',
 				  },
